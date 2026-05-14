a4bf8dc25709d2b42964782009c8d54bafebb805
Developer <contato.luands@gmail.com>
2026-05-14 01:34:00 -0300

    feat: add postcss configuration with tailwindcss and autoprefixer plugins to admin app

--- apps/admin/postcss.config.js
+++ apps/admin/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    [require.resolve('tailwindcss')]: { config: require.resolve('./tailwind.config.js') },
-    [require.resolve('autoprefixer')]: {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
